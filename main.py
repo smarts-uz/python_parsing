76e5ca70346eb0d18fc06ba1dd117257e2d4807c
Diyorbek Azimqulov <azimqulovdiyorbek@gmail.com>
2021-08-18 14:34:58 +0500

    images and thumbnails added to product

--- main.py
+++ main.py
@@ -1,44 +1,140 @@
+from genericpath import isdir, isfile
+import os
+from bs4 import BeautifulSoup
 import requests
 
-from bs4 import BeautifulSoup
+def remove_incident_file(parent_folder):
+    """
+        removes all incident id errors, 
+        structure of this parent folder should be parent_folder/folders/pages
+    """
+    dirs = os.listdir(parent_folder)
+    for dir in dirs:
+        if '.' in dir:  # ignoring secret dirs
+            continue
+
+        # ignore files which is in parent dir
+        if not (os.path.isdir(f"{parent_folder}/{dir}")):
+            continue
+        
+        files = os.listdir(f'{parent_folder}/{dir}')
+        for file in files:
+            
+            # if inside of folder there is another folder ignore that folder
+            if not(os.path.isfile(f"{parent_folder}/{dir}/{file}")):
+                continue
+            with open(f"{parent_folder}/{dir}/{file}", 'r') as f:
+                try:
+                    content = f.read()
+                    if ('Incapsula' in content):
+                        os.remove(f"{parent_folder}/{dir}/{file}")
+                except UnicodeDecodeError:
+                    # remove not relevant file
+                    os.remove(f"{parent_folder}/{dir}/{file}")
 
 
-class YandexMarketProductDetail:
+def get_file_content(path):
+    with open(path, 'r') as fl:
+        content = fl.read()
+        if (len(content) < 5):
+            return None
+        return content
 
-    def __init__(self, file_path) -> None:
-        self.file_path = file_path
 
-    def fetch(self):
-        with open(self.file_path, 'r') as fl:
-            html = fl.read()
-        return html
-    
-    def parse(self):
-        html = self.fetch()
+
+
+def get_image_url(product_path):
+    html = get_file_content(path=product_path)
+    try:
         soup = BeautifulSoup(html, 'lxml')
-        description = soup.select_one('div._1uLae').text
-        divisions = soup.select('div.la3zd')
-        data = {}
-        dic_list = []
-        for division in divisions:
-            title = division.find('h2').text
-            keys_object = division.find_all('span')
-            values_object = division.find_all('dd')
-            # removing empty text element
-            keys = [key.text for key in keys_object if key.text != '']
-            values = [value.text for value in values_object if value.text != '']
-            dt = {}
-            for i in range(len(keys)):
-                dt[keys[i]] = values[i]
-            dic = {
-                "title": title,
-                "data": [dt]
-            }
-            dic_list.append(dic)
-        data["description"] = description
-        data["data"] = dic_list
-        with open(f"{self.file_path.split('.')[0]}_data.txt", 'w') as fl:
-            fl.write(str(data))
+        image_div = soup.select_one('div.e0u1')
+        image_url = image_div.select_one('div.e9r7 img[src]')['src']
+        return image_url
+    except:
+        print(html, '\n\n', product_path)
 
-obj = YandexMarketProductDetail(file_path='yandex_a50_detail.html')
-obj.parse()
+
+def download_image(url, path_to_store, image_name):
+    """
+        downloads image according to given url and stores it into given path with the name of image.png
+    """
+    response = requests.get(url)
+
+    file = open(f"{path_to_store}/{image_name}.png", "wb")
+    file.write(response.content)
+    file.close()
+
+
+def get_thumbnail_urls(product_path):
+    html = get_file_content(path=product_path)
+    soup = BeautifulSoup(html, 'lxml')
+    thumbnails = soup.select('div.e9q6 img._3Ugp')
+    thumbnail_urls = [t['src'] for t in thumbnails]
+    return thumbnail_urls
+
+
+
+def add_image_to_product(product_path, product_folder):
+    """
+        it stores images and thumbnails according to given product
+    """
+    image_url = get_image_url(product_path=product_path)
+    image_folder = f'{product_folder}/image'
+    thumbnail_urls = get_thumbnail_urls(product_path=product_path)
+    thumbnail_folder = f'{product_folder}/thumbnails'
+    # make a directory to store the image
+    try: 
+        os.mkdir(image_folder) 
+        os.mkdir(thumbnail_folder)
+
+        download_image(url=image_url, path_to_store=image_folder, image_name='image')
+
+    # download thumbnail images and save it
+        if (len(thumbnail_urls) > 0):
+            ctr = 1
+            for thumbnail_url in thumbnail_urls:
+                url = thumbnail_url.replace('c50', 'c1000')
+                download_image(url=url, path_to_store=thumbnail_folder, image_name=f'thumbnail{ctr}')
+                ctr += 1
+
+    except OSError as error: 
+        print(error)  
+    
+    # download a product image and save it
+    
+
+
+# add_image_to_product(product_path='product/mobilnyy-telefon-vertex-k213-chernyy-serebristyy-199643880/@asb=BCRBR5_25252FCrku5mPfrucePsyqOfn4Uvu2HFq2QcQvVDJU_25253D&asb2=gdfsE5Sz90hB7s3vXLH1veU_ekwB3MNn8aK1sJS1-qk', 
+#                     product_folder='product/mobilnyy-telefon-vertex-k213-chernyy-serebristyy-199643880'
+#                     )
+
+def add_images(parent_folder):
+    """
+        adds images and thumbnails to a certain product
+        structure of this parent folder should be parent_folder/folders/pages
+    """
+    dirs = os.listdir(parent_folder)
+    
+    # ignoring secret dirs
+    for dir in dirs:
+        if '.' in dir:  
+            continue
+        
+        # ignore files which is in parent dir
+        if not (os.path.isdir(f"{parent_folder}/{dir}")):
+            continue
+
+        files = os.listdir(f'{parent_folder}/{dir}')
+        for file in files:
+            
+            # if inside of folder there is another folder ignore that folder
+            if not(os.path.isfile(f"{parent_folder}/{dir}/{file}")):
+                continue
+            
+            add_image_to_product(product_path=f"{parent_folder}/{dir}/{file}", 
+                                product_folder=f'{parent_folder}/{dir}'
+                                )
+
+remove_incident_file('product')
+add_images('product')
+
